e9f4abe0416095ac653946c8a2c56c42bc2b291a
Henrik Volmari <henrik.volmari@gmail.com>
2020-11-15 17:10:28 +0100

    Added date feature

--- website/scriptOverview.js
+++ website/scriptOverview.js
@@ -16,6 +16,18 @@ async function getUserTransactions() {
   return userTransactions;
 }
 
+async function getUserTransactionsByMonth(startDate, endDate) {
+  const allUserTransactions = await getUserTransactions();
+  let userTransactionsByMonth = [];
+  allUserTransactions.forEach((transaction) => {
+    const transactionDate = new Date(transaction.date);
+    if (transactionDate >= startDate && transactionDate <= endDate) {
+      userTransactionsByMonth.push(transaction);
+    }
+  });
+  return userTransactionsByMonth;
+}
+
 async function transactionsToUI(userTransactions) {
   const transactionsPosition = document.querySelector("#transactions");
   for (const userTransaction of await userTransactions) {
@@ -40,7 +52,7 @@ function createTransaction(data) {
   deleteButton.addEventListener(
     "click",
     function () {
-      deleteTransactionFromJson(data.id);
+      deleteTransactionFromJson(data.id, data.date);
     },
     false
   );
@@ -52,15 +64,20 @@ function createTransaction(data) {
   const titleP = document.createElement("p");
   titleP.classList.add("title");
 
+  const dateSpan = document.createElement("span");
+  dateSpan.classList.add("title");
+
   transactionDiv.appendChild(amountP);
   transactionDiv.appendChild(deleteButton);
   transactionDiv.appendChild(categoryP);
   transactionDiv.appendChild(titleP);
+  transactionDiv.appendChild(dateSpan);
 
   amountP.textContent = data.amount + "€";
   deleteButton.textContent = "delete";
   categoryP.textContent = data.category;
   titleP.textContent = data.title;
+  dateSpan.textContent = data.date;
 
   return transactionDiv;
 }
@@ -69,29 +86,46 @@ function addTransaction(event) {
   const inputData = document.forms[0].elements;
   const inputTitle = inputData["input_title"].value;
   const inputType = typeInput;
+  const inputDate = inputData["input_date"].value;
   const inputAmount = inputData["input_amount"].value;
   const inputCategory = inputData["input_category"].value;
   const transactionId = generateId();
 
-  if (!inputTitle || !inputAmount) {
-    alert("Watch out! - The title and/or amount is missing.");
-  } else if (inputTitle.length > 20) {
-    alert("Watch out! - The title is too long (max=20digits).");
-  } else {
-    if (inputType) {
-      amount = inputAmount;
-    } else {
-      amount = "-".concat(inputAmount);
-    }
-    postTransactionToJson({
-      title: inputTitle,
-      amount: amount,
-      category: inputCategory,
-      user: localStorage.getItem("username"),
-      id: transactionId,
-    });
+  try {
+    if (!inputTitle) throw "The title is missing.";
+    if (inputTitle.length > 30) throw "The title is too long (max=30digits).";
+    if (!inputDate) throw "The date is missing.";
+    if (!/\d{4}-\d{2}-\d{2}/.test(inputDate)) throw "The date format is wrong. -> Format: YYYY-MM-DD";
+    if (!inputAmount) throw "The amount is missing.";
+    inputAmountNumber = Number(inputAmount);
+    if (inputAmountNumber < 0) throw "The amount can't be negativ.";
+    if (!inputCategory) throw "The category is missing.";
+    if (inputTitle.length > 20) throw "The category is too long (max=20digits).";
+  } catch (err) {
+    alert("Error: " + err);
+    return;
   }
+  // if (!inputTitle || !inputAmount) {
+  //   alert("Watch out! - The title and/or amount is missing.");
+  // } else if (inputTitle.length > 20) {
+  //   alert("Watch out! - The title is too long (max=20digits).");
+  // } else {
+  if (inputType) {
+    amount = inputAmount;
+  } else {
+    amount = "-".concat(inputAmount);
+  }
+
+  postTransactionToJson({
+    title: inputTitle,
+    amount: amount,
+    category: inputCategory,
+    user: localStorage.getItem("username"),
+    id: transactionId,
+    date: inputDate,
+  });
 }
+//}
 
 async function postTransactionToJson(obj) {
   const res = await fetch("http://localhost:3000/transactions", {
@@ -104,10 +138,10 @@ async function postTransactionToJson(obj) {
     body: JSON.stringify(obj),
   });
 
-  updateAllData();
+  showSpezificMonth(obj.date); //Monat der Eingabe übergeben
 }
 
-async function deleteTransactionFromJson(id) {
+async function deleteTransactionFromJson(id, date) {
   const res = await fetch("http://localhost:3000/transactions/" + id, {
     method: "DELETE",
     headers: {
@@ -116,10 +150,15 @@ async function deleteTransactionFromJson(id) {
     },
   });
 
-  updateAllData();
+  showSpezificMonth(date); //aktuelle Anzeige lassen
 }
 
-function updateAllData() {
+async function updateAllData(date) {
+  const startOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+  const endOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 1); //vielleicht auslagern
+  console.log(startOfMonth);
+  console.log(endOfMonth);
+
   const currentTransactions = document.querySelector("#transactions");
   const currentCategories = document.querySelector("#categories");
   const currentBalance = document.querySelector("#total-table");
@@ -127,10 +166,12 @@ function updateAllData() {
   clearChildren(currentCategories);
   clearChildren(currentBalance);
 
-  const updatedTransactions = getUserTransactions();
+  const updatedTransactions = getUserTransactionsByMonth(startOfMonth, endOfMonth);
   transactionsToUI(updatedTransactions);
   groupAmountByExpenseCategory(updatedTransactions);
-  displayBalance(updatedTransactions);
+  displayBalance(await getUserTransactions());
+  displayTotalExpenseAndProfit(updatedTransactions);
+  setExpense();
 }
 
 function clearChildren(element) {
@@ -140,8 +181,8 @@ function clearChildren(element) {
   }
 }
 
-function switchCategoryOptionsTo(arr) {
-  const allOptions = document.querySelector("#category-options"); //("#input_category");
+function switchDropdownOptionsTo(arr, dropdownName) {
+  const allOptions = document.querySelector(dropdownName);
   clearChildren(allOptions);
 
   let option = [];
@@ -157,13 +198,33 @@ function switchCategoryOptionsTo(arr) {
   }
 }
 
+function setDayDropdown(month) {
+  const daysFeb = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29"];
+  const daysShort = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"];
+  const daysLong = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+
+  if (month === "February") {
+    switchDropdownOptionsTo(daysFeb, "#input_day");
+  } else if (month === "April" || month === "June" || month === "September" || month === "November") {
+    switchDropdownOptionsTo(daysShort, "#input_day");
+  } else if (month === "January" || month === "March" || month === "May" || month === "July" || month === "August" || month === "October" || month === "December") {
+    switchDropdownOptionsTo(daysLong, "#input_day");
+  }
+
+  // if (month === "February") {
+  //   document.getElementById("30").style.display = "none";
+  //   //document.getElementById("30").remove();
+  //   document.getElementById("31").remove();
+  //}
+}
+
 function setExpense() {
   typeInput = false;
   document.getElementById("expense-button").className = "expense-button-on";
   document.getElementById("profit-button").className = "profit-button-off";
 
   const expenseOptions = ["Groceries", "Restaurant", "Entertainment", "Travel", "Education", "Clothes", "other..."];
-  switchCategoryOptionsTo(expenseOptions);
+  switchDropdownOptionsTo(expenseOptions, "#category-options");
 }
 function setProfit() {
   typeInput = true;
@@ -171,7 +232,7 @@ function setProfit() {
   document.getElementById("profit-button").className = "profit-button-on";
 
   const profitOptions = ["Salary", "Pocket Money", "Present", "Casino", "Found", "Stolen", "other..."];
-  switchCategoryOptionsTo(profitOptions);
+  switchDropdownOptionsTo(profitOptions, "#category-options");
 }
 
 function onlyUnique(value, index, self) {
@@ -270,9 +331,31 @@ function createCategory(data) {
   return categoryDiv;
 }
 
-async function displayBalance(transactions) {
-  let userTransactions = await transactions;
+async function displayBalance(allTransactions) {
+  let transactions = await allTransactions;
   let totalBalance = 0;
+  let totalProfit = 0;
+  let totalExpense = 0;
+
+  transactions.forEach((transaction) => {
+    amount = parseFloat(transaction.amount);
+    if (amount < 0) {
+      totalExpense += amount;
+    } else {
+      totalProfit += amount;
+    }
+  });
+  totalBalance = totalExpense + totalProfit;
+
+  const totalTable = document.querySelector("#total-table");
+  const balanceRow = createTableRow("Balance", totalBalance);
+  totalTable.appendChild(balanceRow);
+}
+
+async function displayTotalExpenseAndProfit(transactions) {
+  //balance soll auf ganzen Zeitraum bleiben (neue funktion)
+  let userTransactions = await transactions;
+  //let totalBalance = 0;
   let totalProfit = 0;
   let totalExpense = 0;
 
@@ -284,15 +367,15 @@ async function displayBalance(transactions) {
       totalProfit += amount;
     }
   });
-  totalBalance = totalExpense + totalProfit;
+  //totalBalance = totalExpense + totalProfit;
 
   const totalTable = document.querySelector("#total-table");
 
-  const balanceRow = createTableRow("Balance", totalBalance);
+  //const balanceRow = createTableRow("Balance", totalBalance);
   const expenseRow = createTableRow("Expense", totalExpense);
   const profitRow = createTableRow("Profit", totalProfit);
 
-  totalTable.appendChild(balanceRow);
+  //totalTable.appendChild(balanceRow);
   totalTable.appendChild(expenseRow);
   totalTable.appendChild(profitRow);
 }
@@ -328,9 +411,61 @@ function generateId() {
   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) => (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16));
 }
 
+function showSpezificMonth(date) {
+  const spezificDate = new Date(date);
+
+  displayMonth(spezificDate);
+  updateAllData(spezificDate);
+}
+function showPreviousMonth(date) {
+  const previousDate = new Date(date);
+  previousDate.setMonth(previousDate.getMonth() - 1);
+
+  displayMonth(previousDate);
+  updateAllData(previousDate);
+}
+function showNextMonth(date) {
+  const nextDate = new Date(date);
+  nextDate.setMonth(nextDate.getMonth() + 1);
+
+  displayMonth(nextDate);
+  updateAllData(nextDate);
+}
+function showCurrentMonth() {
+  const currentDate = new Date();
+
+  displayMonth(currentDate);
+  updateAllData(currentDate);
+}
+
+function displayMonth(date) {
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Dezember"];
+  document.getElementById("month").textContent = date.getFullYear() + " " + months[date.getMonth()];
+  document.getElementById("hidden_date").textContent = date;
+}
+
+function groupProfitCategoryByMonth(date) {
+  const currentDate = new Date(date);
+  const startOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+  const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1);
+
+  const userTransactionsByMonth = getUserTransactionsByMonth(startOfMonth, endOfMonth);
+  groupAmountByProfitCategory(userTransactionsByMonth);
+}
+function groupExpenseCategoryByMonth(date) {
+  const currentDate = new Date(date);
+  const startOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+  const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1);
+
+  const userTransactionsByMonth = getUserTransactionsByMonth(startOfMonth, endOfMonth);
+  groupAmountByExpenseCategory(userTransactionsByMonth);
+}
+
 //let typeInput = false;
 
-transactionsToUI(getUserTransactions());
-groupAmountByExpenseCategory(getUserTransactions());
-displayBalance(getUserTransactions());
-setExpense();
+// transactionsToUI(getUserTransactions(startOfCurrentMonth, endOfCurrentMonth));
+// groupAmountByExpenseCategory(getUserTransactions(startOfCurrentMonth, endOfCurrentMonth));
+// displayBalance(getUserTransactions(startOfCurrentMonth, endOfCurrentMonth));
+//setExpense();
+
+showCurrentMonth();
